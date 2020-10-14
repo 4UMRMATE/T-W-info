22db9999cb4ae73d149d8250d4f997fefe6291f8
4UMRMATE <sich.mate@gmail.com>
2020-10-14 05:53:51 +0400

    v2.0 -Updated Icons -Added Function to send request for specific city and etc. -Added Demo Footer
    !!! MS !!!

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 feather.replace(); // Feather Icons
 
 let API = '61fd11eeb0d574f59074a1d50f7cb18d';
-document.addEventListener('DOMContentLoaded', () =>{
+document.addEventListener('DOMContentLoaded', () => {
     let city = "Tbilisi";
 
-    let textInput = document.getElementById('lb'); // Display Text Input and Vice Versa
+    let locChange = document.getElementById('lb'); // Display Text Input and Vice Versa
     let enterCityName = document.getElementById('send-button');
-    
-    textInput.addEventListener('click', () => {
+    let textInput = document.getElementById('textInput');
+
+    locChange.addEventListener('click', () => {
         document.getElementById('lic').style.cssText = 'display: flex;';
         document.getElementById('lb').style.cssText = 'display: none;';
     });
@@ -17,6 +18,16 @@ document.addEventListener('DOMContentLoaded', () =>{
         city = document.querySelector('#textInput').value;
         fetchData();
     });
+
+    document.getElementById("textInput").onkeypress = function(event){
+        city = document.querySelector('#textInput').value;
+        if (event.keyCode == 13 || event.which == 13){
+            fetchData();
+            document.getElementById('lb').style.cssText = 'display: flex;';
+            document.getElementById('lic').style.cssText = 'display: none;';
+            document.querySelector('#textInput').value = "";
+        }
+    };
 
     function fetchData(){
         fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API}&units=metric`)
@@ -31,7 +42,7 @@ document.addEventListener('DOMContentLoaded', () =>{
             document.getElementById('location').innerHTML = data.name+", "+data.sys.country;
             let fixedTemp = data.main.temp.toFixed(0); // fix decimal number
             document.getElementById('weather-temp').innerHTML = fixedTemp+"°C";
-            document.getElementById('temp1').innerHTML = data.main.temp+"°C";
+            document.getElementById('temp1').innerHTML = fixedTemp+"°C";
             document.getElementById('weather-desc').innerHTML = data.weather[0].description;
             document.getElementById('visibility').innerHTML = (data.visibility/1000)+" km";
             document.getElementById('humidity').innerHTML = data.main.humidity+" %";
